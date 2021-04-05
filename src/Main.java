0bbd33a19b18bfc2f0a818766ab4c357c25ab757
Oliver Werner <oow@ruc.dk>
2021-04-05 20:42:50 +0200

    Indeholder hele datasættet

--- src/Main.java
+++ src/Main.java
@@ -38,6 +38,46 @@ public class Main {
         Haslev.addOutEdge(new Edge(Haslev, Næstved, 25));
         Haslev.addOutEdge(new Edge(Haslev, Ringsted, 19));
         Haslev.addOutEdge(new Edge(Haslev, Roskilde, 47));
+        Haslev.addOutEdge(new Edge(Haslev, Slagelse, 38));
+        Haslev.addOutEdge(new Edge(Haslev, Sorø, 34));
+        Haslev.addOutEdge(new Edge(Haslev, Vordingborg, 40));
+        Holbæk.addOutEdge(new Edge(Holbæk, Jærgerspris, 34));
+        Holbæk.addOutEdge(new Edge(Holbæk, Kalundborg, 44));
+        Holbæk.addOutEdge(new Edge(Holbæk, Korsør, 66));
+        Holbæk.addOutEdge(new Edge(Holbæk, Ringsted, 36));
+        Holbæk.addOutEdge(new Edge(Holbæk, Roskilde, 32));
+        Holbæk.addOutEdge(new Edge(Holbæk, Slagelse, 46));
+        Holbæk.addOutEdge(new Edge(Holbæk, Sorø, 34));
+        Jærgerspris.addOutEdge(new Edge(Jærgerspris, Korsør, 95));
+        Jærgerspris.addOutEdge(new Edge(Jærgerspris, Køge, 58));
+        Jærgerspris.addOutEdge(new Edge(Jærgerspris, Ringsted, 56));
+        Jærgerspris.addOutEdge(new Edge(Jærgerspris, Roskilde, 33));
+        Jærgerspris.addOutEdge(new Edge(Jærgerspris, Slagelse, 74));
+        Jærgerspris.addOutEdge(new Edge(Jærgerspris, Sorø, 63));
+        Kalundborg.addOutEdge(new Edge(Kalundborg, Ringsted, 62));
+        Kalundborg.addOutEdge(new Edge(Kalundborg, Roskilde,70));
+        Kalundborg.addOutEdge(new Edge(Kalundborg, Slagelse, 39));
+        Kalundborg.addOutEdge(new Edge(Kalundborg, Sorø, 51));
+        Korsør.addOutEdge(new Edge(Korsør, Næstved, 45));
+        Korsør.addOutEdge(new Edge(Korsør, Slagelse, 20));
+        Køge.addOutEdge(new Edge(Køge, Næstved, 45));
+        Køge.addOutEdge(new Edge(Køge, Ringsted, 28));
+        Køge.addOutEdge(new Edge(Køge, Roskilde, 25));
+        Køge.addOutEdge(new Edge(Køge, Vordingborg, 60));
+        Maribo.addOutEdge(new Edge(Maribo, Nakskov, 27));
+        Maribo.addOutEdge(new Edge(Maribo, Nykøbing,  26));
+        Næstved.addOutEdge(new Edge(Næstved, Roskilde, 57));
+        Næstved.addOutEdge(new Edge(Næstved, Ringsted, 26));
+        Næstved.addOutEdge(new Edge(Næstved, Slagelse, 37));
+        Næstved.addOutEdge(new Edge(Næstved, Sorø, 32));
+        Næstved.addOutEdge(new Edge(Næstved, Vordingborg, 28));
+        Ringsted.addOutEdge(new Edge(Ringsted, Roskilde, 31));
+        Ringsted.addOutEdge(new Edge(Ringsted, Sorø, 15));
+        Ringsted.addOutEdge(new Edge(Ringsted, Vordingborg, 58));
+        Slagelse.addOutEdge(new Edge(Slagelse, Sorø, 14));
 
+        graph.printGraph();
     }
+
+
 }
